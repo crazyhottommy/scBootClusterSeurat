c6f802394066aa9e36073cc6b8de8632b978f2cd
Ming Tang <tangming2005@gmail.com>
2018-12-11 15:04:57 -0500

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # scBootClusterSeurat
 A snakemake pipeline to scatter and gather bootstrapped Seurat@ident
+
+on `odyssey` cluster(SLURM):
+
+```bash
+# R3.5.1
+module load R
+
+#hdf5
+module load hdf5
+
+R
+>install.package("Seurat")
+
+```
---
+++ scripts/bootstrap_cluster.R
@@ -0,0 +1,10 @@
+library(scclusteval)
+
+seurat_obj<- snakemake@input[[1]]
+PreprocessSubsetData_pars<- snakemake@params[["PreprocessSubsetData_pars"]]
+subset_seurat_obj<- RandomSubsetData(seurat_obj, rate = snakemake@params[["rate"]])
+subset_seurat_obj<- PreprocessSubsetData(subset_seurat_obj, PreprocessSubsetData_pars)
+
+res<- list(ident = object@ident, pc.sig = object@meta.data$pc.sig))
+run_id<- snakemake@wildcards[["run_id"]]
+save(res, file = paste0("bootstrap_cluster/bootstrap_cluster_", run_id, ".rda" )
